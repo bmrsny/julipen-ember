00780e40c74f41c82d9b2c38db53d7483eb791bd
Brandon Mrsly <bmrsny@gmail.com>
2016-01-22 16:27:12 -0700

    add http mock and project boiler plate code

--- app/projects/route.js
+++ app/projects/route.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+ model: function(params) {
+   return this.store.findAll('project');
+ },
+ actions: {
+   addProject: function() {
+     var project = this.store.createRecord('project', {
+       title: this.controller.get('title'),
+       description: this.controller.get('description')
+     });
+     project.save().then(() => {
+       console.log('save successful');
+       this.controller.set('title',null);
+       this.controller.set('description',null);
+       this.refresh();
+     }, function() {
+       console.log('save failed');
+     });
+   }
+ }
 });
