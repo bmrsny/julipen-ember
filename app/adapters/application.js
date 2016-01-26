0936b723fdc24249704d775f90e311bf639713e3
Brandon Mrsly <bmrsny@gmail.com>
2016-01-26 11:07:38 -0700

    add in route mixins for authenticated, unathenticated, and applicationMixin

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+// import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:application',
+export default DS.RESTAdapter.extend({
+  // authorizer: 'authorizer:application',
   namespace: 'api',
 });
