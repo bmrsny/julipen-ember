5e2f7760c97936b40cc8e14a90c8abdfa9487619
Julian Coutu <juliancoutu@Julians-MacBook-Pro.local>
2016-01-29 02:34:05 -0500

    Adds required needs to the model unit tests

--- tests/unit/models/shot-test.js
+++ tests/unit/models/shot-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('shot', 'Unit | Model | shot', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ["model:project", "model:user", "model:comment"]
 });
 
 test('it exists', function(assert) {
