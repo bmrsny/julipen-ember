26e8430e67e830667e0fc15d2be5b6674a1982b9
Brandon Mrsly <bmrsny@gmail.com>
2016-01-28 18:59:18 -0700

    assumptions were correct, need to load all shots for all models(projects)

--- app/projects/project/route.js
+++ app/projects/project/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.query('shot', { project: params.project_id });
+    return this.store.findAll('shot', { project: params.project_id });
   },
 
   actions: {
